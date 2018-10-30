2e3e06a8c5a3783772897b950e357c734b2b4d61
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-10-30 11:10:13 -0600

    got the serializer working, my server wasnt on this whole time LOL. working on formatting the serializer

--- spec/requests/games_request_spec.rb
+++ spec/requests/games_request_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 describe 'as a guest user' do
   it 'should return game and score data in specific format' do
-    get '/api/v1/games/1'
-    expect(response).to be_successful
+    game = create(:game)
 
+    get "/api/v1/games/#{game.id}"
+    expect(response).to be_successful
+    
     details = JSON.parse(response.body)
 
-    expect(details[:game_id]).to eq(1)
+    expect(details[:game_id]).to eq(game.id)
     expect(details[:scores]).to be_an(Array)
     expect(details[:scores].count).to eq(2)
   end
