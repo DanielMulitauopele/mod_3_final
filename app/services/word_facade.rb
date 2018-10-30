c8f497b5064d5701172aec5e857288ee5edcb3fa
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-10-30 09:48:02 -0600

    working on getting faraday connected, forgot some of the details for inserting headers

--- app/services/word_facade.rb
+++ app/services/word_facade.rb
@@ -4,4 +4,20 @@ class WordFacade
   def initialize(word)
     @word = word
   end
+
+  def sentences
+
+  end
+  
+  def conn
+    Faraday.new(url: 'https://od-api.oxforddictionaries.com') do |faraday|
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
+
+  def result
+    response = conn.get("/api/v1/entries/en/#{@word}/sentences")
+    a = JSON.parse(response.body, symbolize_names: true)
+    require "pry"; binding.pry
+  end
 end
