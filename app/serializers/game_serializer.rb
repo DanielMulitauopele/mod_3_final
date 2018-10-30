46eda174d661266ed72a0c98f5301b75b04864fd
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-10-30 11:19:59 -0600

    working on formatting player 1 and player 2 data

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,11 +1,24 @@
 class GameSerializer < ActiveModel::Serializer
   attributes :game_id, :scores
 
+  has_many :plays
+  belongs_to :player_1
+  belongs_to :player_2
+
   def game_id
-    :id
+    object.id
   end
 
   def scores
-    
+    require "pry"; binding.pry
+    [player_1, player_2]
+  end
+
+  def player_1
+    {user_id: object.player_1_id}
+  end
+
+  def player_2
+    {user_id: object.player_2_id}
   end
 end
